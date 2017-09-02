fe909293cc1d943e46b44028579370e8a6921e0d
Alan Johnson <skittlyoaten@users.noreply.github.com>
2017-09-02 09:45:16 -0500

    Add files via upload
    cleanup, style update

--- js/flyout.js
+++ js/flyout.js
@@ -3,8 +3,8 @@
  */
 
 // PLACEHOLDER DATA - generating a list of social "share this" links as data for developemnt purposes.
-var flyout_urlPath = encodeURIComponent(window.location.href)
-var flyout_title = encodeURIComponent(document.title)
+var flyout_urlPath = encodeURIComponent(window.location.href);
+var flyout_title = encodeURIComponent(document.title);
 
 data_elements = {
     // can be any object of data we choose to pass to it.
@@ -84,16 +84,18 @@ data_elements = {
         name: 'Stumble Upon',
         url : 'http://www.stumbleupon.com/submit?url=' + flyout_urlPath + '&flyout_title=' + flyout_title
     }
-}
+};
 // END PLACEHOLDER DATA
 
 // just add easing we want without grabbing the whole library
 $.extend($.easing, {
   easeOutElastic: function (x, t, b, c, d) {
     var s=1.70158;var p=0;var a=c;
-    if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
-    if (a < Math.abs(c)) { a=c; var s=p/4; }
-    else var s = p/(2*Math.PI) * Math.asin (c/a);
+    if (t===0) return b;
+    if ((t/=d)==1) return b+c;
+    if (!p) p=d*0.3;
+    if (a < Math.abs(c)) { a=c; s=p/4; }
+    else s = p/(2*Math.PI) * Math.asin (c/a);
     return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
   }
 })
@@ -108,22 +110,20 @@ $.fn.addFlyout = function(options) {
     flyout_theme: 'light'
   }, options);
   var flyout_home = $("<div>", {class: "flyout_home"});
-  var flyout_linkNames = []
-  var visible = false
-  var mousein = false
-  var containerWidth = options.flyout_radius - (options.flyout_size)
-  var containerHeight = options.flyout_radius - (options.flyout_size)
+  var flyout_linkNames = [];
+  var visible = false;
+  var mousein = false;
+  var containerWidth = options.flyout_radius - (options.flyout_size);
+  var containerHeight = options.flyout_radius - (options.flyout_size);
   var center = {
       x: $(flyout_container).width() / 2,
       y: $(flyout_container).height() / 2
-  }
+  };
   flyout_container.append(flyout_home);
-  flyout_home.append('<div class="flyout_tooltip">Share This</div>').addClass(options.flyout_theme);
-
   $.each(options.item_data, function(key, data) {
-    if (data != 0) {
-      flyout_container.append('<a href="'+data.url+'" class="flyout_link '+key+' '+options.flyout_theme+'  " alt="'+data.name+'" data-title="'+data.name+'" style="width:'+options.flyout_size+'px;height:'+options.flyout_size+'px"></a>')
-      flyout_linkNames.push(data.name)
+    if (data !== 0) {
+      flyout_container.append('<a href="'+data.url+'" class="flyout_link '+key+' '+options.flyout_theme+'  " alt="'+data.name+'" data-title="'+data.name+'" style="width:'+options.flyout_size+'px;height:'+options.flyout_size+'px"><div class="flyout_tooltip">'+data.name+'</div></a>');
+      flyout_linkNames.push(data.name);
     }
   });
 
@@ -132,15 +132,19 @@ $.fn.addFlyout = function(options) {
       $(this).css({
           top: center.y - (options.flyout_size/2),
           left: center.x - (options.flyout_size/2)
-      })
-  })
+      });
+  });
   function showLinks() {
-      mousein = true
+      mousein = true;
+      flyout_container.addClass('hover');
       if (!visible) {
-          visible = true
+          visible = true;
           flyout_container.find('.flyout_link').each(function(index) {
-              speed = Math.floor(Math.random() * (1000 - 700 + 1) + 1000)
-              var theta = (2 * Math.PI) * index / flyout_container.find('.flyout_link').length - 1.5 //-1.5 to start at 12:00 position
+              speed = Math.floor(Math.random() * (1000 - 700 + 1) + 1000);
+              var theta = (2 * Math.PI) * index / flyout_container.find('.flyout_link').length - 1.5; //-1.5 to start at 12:00 position
+              if ( ( center.x - (options.flyout_size/2) + (Math.cos(theta)*(options.flyout_radius / 2) ) ) <= 0 ) {
+                $(this).addClass('left');
+              }
               $(this).stop().animate({
                   top: center.y  - (options.flyout_size/2) + (Math.sin(theta)*(options.flyout_radius / 2) ),
                   left: center.x - (options.flyout_size/2) + (Math.cos(theta)*(options.flyout_radius / 2) ),
@@ -148,68 +152,47 @@ $.fn.addFlyout = function(options) {
                   width: (options.flyout_size + 'px'),
                   height: (options.flyout_size + 'px')
               },speed, "easeOutElastic", function() {
-                var myX = $(this).position().left
-                var myY = $(this).position().top
+                var myX = $(this).position().left;
+                var myY = $(this).position().top;
                 $(this).bind('mouseenter', (function() {
-                    mousein = true
-                    $(this).clearQueue()
-                    .animate({
-                        width: options.flyout_size * 1.2,
-                        height: options.flyout_size * 1.2,
-                        top: myY - (((options.flyout_size * 1.2) - options.flyout_size) / 2),
-                        left: myX - (((options.flyout_size * 1.2) - options.flyout_size) / 2)
-                    }, 100).animate({
-                        width: options.flyout_size+'px',
-                        height: options.flyout_size+'px',
-                        top: myY,
-                        left: myX
-                    }, 100)
-                    flyout_container.find(".flyout_tooltip").css({height: $(this).height(), top: $(this).position().top, left: $(this).position().left})
-                    flyout_container.find(".flyout_tooltip").html(flyout_linkNames[index])
-                })).bind('mouseleave', (function() {
-                    $(this)
-                    .animate({
-                        width: options.flyout_size+'px',
-                        height: options.flyout_size+'px'
-                    }, 50)
-                    flyout_container.find("flyout_tooltip").html('Share This')
-                }))
-              })
-          })
+                    mousein = true;
+                })).bind('click', function(e) { e.preventDefault(); });
+              });
+          });
       }
   }
   function hideLinks() {
-      mousein = false
+      mousein = false;
       window.setTimeout(function() {
           if (visible && !mousein) {
-              visible = false
+              visible = false;
+              flyout_container.removeClass('hover');
               flyout_container.find('.flyout_link').each(function() {
-                  speed = Math.floor(Math.random() * (200 - 100 + 1) + 200)
+                  speed = Math.floor(Math.random() * 301);
                   $(this).unbind().stop().animate({
                       top: center.y,
                       left: center.x,
                       opacity: 0,
                       width: 0,
                       height: 0
-                  },speed, "linear")
-              })
+                  },speed, "linear");
+              });
           }
       }, 1000, true);
   }
-  flyout_container.bind('mouseleave', (hideLinks))
-  flyout_home.bind('mouseenter', (showLinks))
+  flyout_container.bind('mouseleave', (hideLinks));
+  flyout_home.bind('mouseenter', (showLinks));
 };
 
 $(document).ready(function() {
 
-  $('.flyout_menu').on('mouseenter', function(e){
-    if ( $(this).has('.flyout_home').length == 0 ) {
+  $('.flyout_menu').bind('mouseenter', function(e){
+    if ( $(this).has('.flyout_home').length === 0 ) {
       $(this).addFlyout({
         item_data: $(this).data('flyoutdata'),
         flyout_radius: $(this).data('flyoutradius'),
         flyout_size: $(this).data('flyoutsize')
-      })
+      });
     }
-  })
-
-})
+  });
+});
